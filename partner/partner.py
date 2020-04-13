e9c51db8357e695e7725ce58149fbf15ff98bd97
Asher <asherlamty@gmail.com>
2020-04-13 11:51:21 +0800

    Update partner.py

--- partner/partner.py
+++ partner/partner.py
@@ -8,6 +8,7 @@ from core.models import PermissionLevel
 class Partner(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.db = bot.plugin_db.get_partition(self)
 
     @commands.group(invoke_without_command=True)
     @checks.has_permissions(PermissionLevel.OWNER)
@@ -20,11 +21,35 @@ class Partner(commands.Cog):
 
     @parnter.command()
     @checks.has_permissions(PermissionLevel.OWNER)
+    async def channel(self, ctx, channel: discord.TextChannel):
+        """
+        Set the partner channel
+        """
+
+        await self.db.find_one_and_update(
+            {"_id": "config"}, {"$set": {"channel": channel.id}}, upsert=True
+        )
+
+        await ctx.send("Partner Channel Set")
+        return 
+
+    @parnter.command()
+    @checks.has_permissions(PermissionLevel.OWNER)
     async def setup(self, ctx):
         if ctx.author.bot:
           return
 
-        server = await self.db.find_one({"id": ctx.channel.guild})
+        channel_config = await self.db.fine_one({"_id": "config"})
+
+        if channel_config is None:
+            return await ctx.send("There's no configured partner channel.")
+        else:
+            channel = ctx.guild.get_channel(int(channel_config["channel"]))
+        
+        if channel is None:
+            return
+
+        server = await self.db.find_one({"guildid": ctx.channel.guild})
 
         if server is None:
         
@@ -35,8 +60,9 @@ class Partner(commands.Cog):
             
             await self.db.insert_one(
               {
-                "id": ctx.channel.guild,
+                "id": ctx.author.id,
                 "name": ctx.author.name,
+                "guildid": ctx.channel.guild,
                 "channelid": ctx.channel.id,
                 "messageid": parnterid,
               })
@@ -45,7 +71,6 @@ class Partner(commands.Cog):
 
             embed=discord.Embed(description="You are our partner already ")
             await ctx.send(embed=embed)
-
-
+    
 def setup(bot):
     bot.add_cog(Partner(bot))
